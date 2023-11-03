e8af9f7416d83e3ac69482570b3b9378051671d5
normanromero06 <norman.romero13607@est.uca.edu.ni>
2023-11-02 19:00:05 -0600

    hello world en Dark

---
+++ 01_hello_world.dart
@@ -0,0 +1,11 @@
+void main(){
+  //var myName = 'Norman';
+  // late final myName;
+  // String myName = 'Norman';
+
+  const myName = 'Norman';
+
+  print('Hola $myName');
+  print('Hola ${myName.toUpperCase()}');
+  print('Hola ${ 1 + 1}');
+}
