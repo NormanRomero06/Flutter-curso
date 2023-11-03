e7680120af3116e7c15ab7c51f8f906f5cfb3557
normanromero06 <norman.romero13607@est.uca.edu.ni>
2023-11-03 17:59:49 -0600

    Variables Dynamic

--- 02_variables.dart
+++ 02_variables.dart
@@ -7,12 +7,24 @@ void main(){
   final List<String> abilities = ['impostor'];
   final sprites = <String>['kuka'];
   
+  dynamic errorMessage = 'Hola';
+  errorMessage = true;
+  errorMessage = [1,2,3,4,5,6];
+  errorMessage = {1,2,3,4,5,6};
+  errorMessage = () => true;
+  errorMessage = null;
+  
+  
+  errorMessage += 1; //este es un error y lo permite
+  
+  
   print("""
   $pokemon
   $hp
   $isAlive
   $abilities
   $sprites
+  $errorMessage
   
   """);
 }
