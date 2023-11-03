9a9064530cf50025526ecbcd6cb929198388b6f0
normanromero06 <norman.romero13607@est.uca.edu.ni>
2023-11-02 19:13:22 -0600

    Variables

---
+++ 02_variables.dart
@@ -0,0 +1,18 @@
+void main(){
+  
+  
+  final String pokemon = 'Ditto';
+  final int hp = 100;
+  final bool isAlive = true;
+  final List<String> abilities = ['impostor'];
+  final sprites = <String>['kuka'];
+  
+  print("""
+  $pokemon
+  $hp
+  $isAlive
+  $abilities
+  $sprites
+  
+  """);
+}
